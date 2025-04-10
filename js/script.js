328a82a2c83c80e1b0dee3879bf88a0f5b216b6d
Ihar <petkov.i@list.ru>
2025-04-10 08:38:03 +0300

    start task

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 "use strict";
-// alert('Hello!');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// const result = confirm("Are you here")
 
-// const answer = prompt("Вам есть 18?", "18");
-// console.log(typeof(answer));
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c]= d;
+
+console.log(personalMovieDB);
 
  
